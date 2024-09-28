74f7626bb6b1056a5d7a1efd2ef064de512c54b6
Pproblems95 <l21212001@tectijuana.edu.mx>
2024-09-27 17:42:51 -0700

    login terminado

--- src/App.jsx
+++ src/App.jsx
@@ -2,109 +2,157 @@ import './assets/App.css'
 import logo from '../images/logonofondo.png'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Placeholder } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
 
 
-const width = window.innerWidth
-const height = window.innerHeight
 
 
 
 function App() {
+  const [loading, SetLoading] = useState(true)
+  const [loginError, SetLoError] = useState(null)
+  const navigate = useNavigate()
+  const [isOpen, SetOpen] = useState(false)
+  const [errorMessage, setError] = useState('Ambos campos deben ser llenados con por lo menos 3 caracteres. Los usuarios no deben ser mayores a 20 caracteres.')
+  
+  useEffect(() => {
+    SetLoading(true)
+    fetch('https://velazduran.com:3000/api/auth/check', {
+      method: 'GET',
+      credentials: 'include',
+      headers:{'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then((res) => {
+      if(res.status === 200){
+        SetLoError(false)
+        console.log('setloerror: ', loginError)
+        console.log(res)
+      }
+      else {
+        SetLoError(true)
+      }
+      
+    })
+
+    .catch(error => console.log(error))
+    .finally(() => {
+      SetLoading(false)
+    })
+    
+  }, [])
+
+  useEffect(() => {
+    if (!loading && !loginError) {
+      console.log( 'loading', loading)
+      console.log('loginError', loginError)
+      navigate('/menu');
+    }
+  }, [loginError, loading]);
  
-  const [userPH, SetUserPH] = useState('El usuario debe ser de 3 caracteres minimo y maximo 20')
-  const [passPH, SetPassPH] = useState('La contraseña debe ser de al menos 3 caracteres')
   const [currentUser, SetUser] = useState({
     username: '',
     password: ''
    })
-  const navigate = useNavigate()
-
-
   
-  
-  return (
-    <>
 
-    
-      <div style={{flexDirection:'row', display:'flex', width: '100%', height: '100%'}} >
-        
-      <div id='light' style={{flex: 1, display:'flex', flexDirection:'column', height:1000,  }}>
-          <div style={{alignSelf:'center'}}>
-          <img src={logo} id='logo' width={250} height={250} alt='logotipo' style={{flex:1, display:'flex'}}></img>
+   if(loading){
+    return(
+      <>
+      <p>loading...</p>
+      </>
+    )
+   }
+   
+    return(
+      <>
+    <main style={{height:'100vh'}} class='d-flex flex-row'>
+        <div style={{background:'white', width:'30vw'}} class='d-flex justify-content-between flex-column'>
+          <div class='justify-content-center d-flex' style={{}}>
+          <img src={logo} class='img-fluid ' alt='logo centro educativo'style={{height:'25vh', width:'20vw',  }}/>
           </div>
-          <div style={{ flex:2, alignContent:'center', alignItems:'center', justifyContent:'center', display:'flex' }}>
-            
-            <p style={{textAlign:'center', flex:1 , color:'black'}}>Una escuela comprometida al futuo de las 
-              nuevas generaciones, así como el desarrollo
-              <br></br>
-              íntegro del ser.
-            </p>
+          <div>
+            <p class='text-center'>Trabajando por ser el futuro <br/> y la mejora del ser.</p>
           </div>
-          <div style={{ flex:1, alignSelf:'center'}}>
-            <button style={{background:'black', color:'white', fontSize:30, height:150, width: 200, borderRadius:30}}>
-              Ayuda
-            </button>
+          <div class='d-flex justify-content-center'>
+          <p/>
           </div>
         </div>
-            
-          
-            <div style={{flex:5, alignContent:'center', flexDirection:'column', display:'flex', paddingTop:100, background:'#55d0b6'}}> 
-            <strong style={{textAlign:'center', fontSize:50,}}>¡Bienvenido!</strong>
-            <div style={{display:'grid', gridTemplateColumns: '1fr', alignSelf:'center',}}>
-            <p style={{textAlign:'center', flex:1, padding:40, fontSize:30, fontWeight:'bold' }}>Usuario</p>
-            <input id='username' placeholder={userPH} style={{flex:1, marginTop:20, borderWidth:3, borderTopColor: 'transparent', borderRightColor:'transparent', 
-              borderLeftColor:'transparent', background:'transparent', borderBottomColor:'black', width: 250, alignSelf:'center',  }}
-              value={currentUser.username} onChange={text => SetUser({
-                ...currentUser,
-                username: text.target.value
-              })}/>
-          </div>
-          <div style={{display:'grid', gridTemplateColumns: '1fr', alignContent:'center', alignSelf:'center'}}>
-            <p style={{textAlign:'center', flex:1, padding:40, fontSize:30, fontWeight:'bold' }}>Contraseña</p>
-             <input id='password' type='text' style={{flex:1, marginTop:20, borderWidth:3, borderTopColor: 'transparent', borderRightColor:'transparent', 
-              borderLeftColor:'transparent', background:'transparent', borderBottomColor:'black', width: 250 , alignSelf:'center'}}
-               value={currentUser.password} onChange={e => SetUser({
-                ...currentUser,
-                password: e.target.value
-               })}/>
-          </div>
-          <div style={{flex:1, marginTop:60, alignSelf:'center'}}>
-            <button style={{background:'black', color:'white', height: 150, width:200, borderRadius:30
-              , fontSize:30,
-            }} onClick={ () => {
-              if (currentUser == null) {
-                
-              }
+        <div style={{background:'#55d0b6', width:'70vw'}} class='d-flex  justify-content-between  flex-column '>
+          <div style={{}} class='d-flex justify-content-center'>
+            <p class='text-center h1'>¡Bienvenido!</p>
 
-              if (currentUser.password != null && currentUser.username != null) {
-                fetch("https://velazduran.com:3000/api/auth/login", {
-                  method: 'POST',
-                  headers: {'Content-Type':'application/json'},
-                  body: JSON.stringify({
-                    username: currentUser.username,
-                    password: currentUser.password
-                  })               
-                })
-                .then(response => response.json())
-                .then(data => {
-                  console.log(data)
-                })
-              }
-              else{
-                console.log('incorrecto')
-              }
-              
-            }}>Iniciar sesión</button>
           </div>
-            </div>
+          <div class='d-flex justify-content-center flex-column  mx-3'>
+            <p class='text-center h3'>Usuario</p>
+            <input  style={{background:'white', opacity:'90%'}} class='m-5 rounded' placeholder='Introduce tu usuario' value={currentUser.username} onChange={e => SetUser({
+              ...currentUser,
+              username: e.target.value
+            })}/>
+          </div>
+          <div class='d-flex justify-content-between flex-column mx-3 '>
+            <p class='text-center m h3'>Contraseña</p>
+            <input  style={{background:'white', marginRight:'10vw', opacity:'90%', }} class='rounded m-5' value={currentUser.password} placeholder='Introduce tu contraseña' onChange={e => SetUser({
+              ...currentUser,
+              password: e.target.value
+            })}/>
+          </div>
+          <button id="button"  class='align-self-center m-4 btn-lg btn btn-block' onClick={() => {
+            if((currentUser.username.length < 3 || currentUser.username.length > 20)  || currentUser.password.length < 3){
+              setError('Ambos campos deben ser llenados con por lo menos 3 caracteres. Los usuarios no deben ser mayores a 20 caracteres.')
+              SetOpen(true)
+            }
+            else{
+              fetch('https://velazduran.com:3000/api/auth/login', {
+                    method: 'POST',
+                    credentials:'include',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                      'username': currentUser.username,
+                      'password': currentUser.password
+                    })
+            })
+              .then(response => response.json())
+              .then((response) => {
+                const error = response.error
+                console.log(error)
+                if(error == true){
+                  setError('Credenciales incorrectas, por favor inténtelo de nuevo.')
+                  SetOpen(true)
+                }
+                else{
+                  navigate('/menu')
+                }
+              })
+              .catch(error => console.log(error))
+          }
             
-        
-      </div>
+            
+            
+          }}>Iniciar sesión</button>
 
-      
-    </>
-  )
+        </div>
+
+        <Modal show={isOpen} >
+          <Modal.Header >
+            <Modal.Title> Advertencia </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{errorMessage}</Modal.Body>
+          <Modal.Footer>
+            <button class='btn btn-lg ' onClick={() => {
+              SetOpen(false)
+            }}>Cerrar</button>
+          </Modal.Footer>
+        </Modal>
+      </main>
+      </>
+    )
+    
+   
+ 
+
+  
+  
 }
 
 export default App
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,48 +3,95 @@ import logo from '../images/logonofondo.png'
 import { Button } from 'react-bootstrap'
 import { FaUserCircle } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 
 function Menu() {
     const navigate = useNavigate()
-    return(
-        <main style={{height:'100vh'}}class='d-flex flex-column justify-content-around ' >
-           <div class='container-fluid d-flex justify-content-between' style={{background:'#55d0b6'}}>
-                <div class = 'd-flex '  >
-                    <FaUserCircle  style={{height:70, width:70, margin:10}}   />    
-                </div>
-                <div class='d-flex align-items-center'>
-                    <strong class='h2'> Menú</strong>
-                </div>
-                <div  class='d-flex align-items-center' >
+    const [data, SetData] = useState(null)
+    const [loading, SetLoading] = useState(true)
+    const [loginError, SetLoError] = useState(null)
+    useEffect(() => {
+        SetLoading(true)
+        fetch('https://velazduran.com:3000/api/users/current', {
+            method: 'GET',
+            credentials:'include',
+            header: {'Content-Type' : 'application/json'}
+        })
+        .then((res) => {return res.json();})
+        .then((res) => {SetData(res); })
+        .catch(error => console.log(error))
+        .finally(() => SetLoading(false))
+    }, [])
+
+    useEffect(() => {
+       if(data != null){
+        console.log(data)
+       }    
+    },[loading])
+
+    
+    if(loading){
+        return(
+            <p>Loading...</p>
+        )
+    }
+    else if (!loading){
+        return(
+            <main style={{height:'100vh'}}class='d-flex flex-column justify-content-around ' >
+               <div class='container-fluid d-flex justify-content-between' style={{background:'#55d0b6'}}>
+                    <div class = 'd-flex '  >
                     
-                    <Button class=' btn' style={{background:'black'}} type='button' onClick={() => {
-                        navigate('/menu/Administrar')
-                    }} >Administrar</Button>
-                    
+                    </div>
+                    <div class='d-flex align-items-center'>
+                        <strong class='h2'> Menú</strong>
+                    </div>
+                    <div  class='d-flex align-items-center' >
+                        
+                        <Button class=' btn' style={{background:'black'}} type='button' onClick={() => {
+                            // navigate('/menu/Administrar')
+                            console.log(data)
+                        }} >Administrar</Button>
+                        
+                    </div>
+                      
                 </div>
-                  
-            </div>
-            <div  class='flex-grow-1 flex-row d-flex justify-content-around'>
-                <div  class='align-items-center align-content-around d-flex ' style={{maxWidth: '50%', height:'auto'}}>
-                    <img src={logo} class='img-fluid' alt='logo de centro educativo' />   
-                </div>
-                <div style={{margin:'2%'}} class='d-flex  flex-column justify-content-center flex-grow-1 '  >
-                    <Button class=' btn' style={{background:'black', marginTop:'10%'}} type='button' onClick={() => {
-                        navigate('/menu/RegistroAlumno')
-                    }} >Registrar alumno</Button>
-                    <Button class=' btn' style={{background:'black', marginTop:'10%'}} type='button' onClick={() => {
-                        navigate('/menu/Alumnos')
-                    }}>Ver alumnos</Button>
-                    <Button class=' btn' style={{background:'black', marginTop:'10%'}} type='button' onClick={() => {
-                        navigate('/menu/Pagos')
-                    }} >Pagos</Button>
-                    <Button class=' btn' style={{background:'black', marginTop:'10%'}} type='button' >Página usuario</Button>
-                </div>
-            </div> 
-        </main>
-        
-    )
+                <div  class='flex-grow-1 flex-row d-flex justify-content-around'>
+                    <div  class='align-items-center align-content-around d-flex ' style={{maxWidth: '50%', height:'auto'}}>
+                        <img src={logo} class='img-fluid' alt='logo de centro educativo' />   
+                    </div>
+                    <div style={{margin:'2%'}} class='d-flex  flex-column justify-content-center flex-grow-1 '  >
+                        <Button class=' btn' style={{background:'black', marginTop:'10%'}} type='button' onClick={() => {
+                            navigate('/menu/RegistroAlumno')
+                        }} >Registrar alumno</Button>
+                        <Button class=' btn' style={{background:'black', marginTop:'10%'}} type='button' onClick={() => {
+                            navigate('/menu/Alumnos')
+                        }}>Ver alumnos</Button>
+                        <Button class=' btn' style={{background:'black', marginTop:'10%'}} type='button' onClick={() => {
+                            navigate('/menu/Pagos')
+                        }} >Pagos</Button>
+                        <Button class=' btn' style={{background:'black', marginTop:'10%'}} type='button' onClick={() => {
+                            fetch('https://velazduran.com:3000/api/auth/logout', {
+                                method: 'DELETE',
+                                credentials: 'include'
+                            })
+                            .then(res => res.json())
+                            // .then(res => console.log(res))
+                            .then(() => {
+                                const timeoutID = setTimeout(() => {
+                                    navigate('/')
+                                }, 1000);
+                            })
+                            .catch(error => console.log(error))
+                        }} >Cerrar sesión</Button>
+                    </div>
+                </div> 
+            </main>
+            
+        )
+    }
+
+    
 }
 
 export default Menu
